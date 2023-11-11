46f8aedb6a32c664ffc6b738ea0c2bbfc6c7147c
josealonso <jose.alonso.programmer@gmail.com>
2023-11-11 17:40:18 +0100

    Kotlin does not have built-in support for currying, but you can still create curried functions using higher-order functions and closures.

--- Programming Basics/Functions/Exercise 2/src/Task.kt
+++ Programming Basics/Functions/Exercise 2/src/Task.kt
@@ -1,8 +1,26 @@
 // Functions/Task2.kt
 package functionsExercise2
 
-fun getSum(a: Double, b: Double, c: Double): Double = a + b + c
+fun getSum2(a: Double, b: Double, c: Double): Double = a + b + c
 
 fun main() {
-  println(getSum(1.0, 2.2, 3.4))  // 6.6
+  println(getSum2(1.0, 2.2, 3.4))  // 6.6
+  val res = sumCurried(2)(6)
+  println(res)
+  println(sumCurried2(2)(8))
 }
+
+// Curried function
+fun sumNotCurried(a: Int, b: Int) = a + b
+
+// Source: https://blog.devgenius.io/currying-in-kotlin-9be88bcc930d
+fun sumCurried(a: Int): (Int) -> Int {
+  return fun(b: Int): Int {
+    return a + b
+  }
+}
+
+fun sumCurried2(a: Int): (Int) -> Int =
+  fun(b: Int): Int = a + b
+
+
