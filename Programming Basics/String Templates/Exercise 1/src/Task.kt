e5e066e8aa7335c2d373d26df9c788f60efc6bf3
josealonso <jose.alonso.programmer@gmail.com>
2023-11-11 18:14:49 +0100

    String interpolation is similar to Scala's.

--- Programming Basics/String Templates/Exercise 1/src/Task.kt
+++ Programming Basics/String Templates/Exercise 1/src/Task.kt
@@ -7,5 +7,6 @@ fun findMax(first: Int, second: Int): Int =
 fun main() {
   val first = 17
   val second = 31
-  TODO()
+  val max = findMax(first, second)
+  println("The maximum of $first and $second is $max.")
 }
