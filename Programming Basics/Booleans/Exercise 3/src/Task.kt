3f39a050afc24e1641c6057efc0d45e0a69dcf14
josealonso <jose.alonso.programmer@gmail.com>
2023-11-13 23:17:33 +0100

    Basic exercises with primitive data types and boolean expressions.

--- Programming Basics/Booleans/Exercise 3/src/Task.kt
+++ Programming Basics/Booleans/Exercise 3/src/Task.kt
@@ -2,10 +2,10 @@
 package booleansExercise3
 
 fun and(b1: Boolean, b2: Boolean): Boolean =
-  TODO()
+  if (!b1) false else b2
 
 fun or(b1: Boolean, b2: Boolean): Boolean =
-  TODO()
+  if (b1) true else b2
 
 fun main() {
   println(and(true, true)) // true
--- Programming Basics/Number Types/Exercise 4/src/Task.kt
+++ Programming Basics/Number Types/Exercise 4/src/Task.kt
@@ -1,8 +1,10 @@
 // NumberTypes/Task4.kt
 package numberTypesExercise4
 
+val MILLISECONDS_IN_A_SECOND = 1000L
+val SECONDS_IN_A_MINUTE = 60L
 fun convertToMilliseconds(hours: Int, minutes: Int, seconds: Int): Long =
-  TODO()
+  (seconds + SECONDS_IN_A_MINUTE * (minutes + hours * 60L)) * MILLISECONDS_IN_A_SECOND
 
 fun main() {
   println(convertToMilliseconds(1, 30, 0))
