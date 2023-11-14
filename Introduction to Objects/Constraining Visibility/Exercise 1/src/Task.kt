098ebb02c41c6d3b9c371705d4364e339cc899f5
josealonso <jose.alonso.programmer@gmail.com>
2023-11-14 16:26:42 +0100

    Functions and values/variables are public by default.

--- Introduction to Objects/Constraining Visibility/Exercise 1/src/Task.kt
+++ Introduction to Objects/Constraining Visibility/Exercise 1/src/Task.kt
@@ -1,10 +1,27 @@
 // Visibility/Task1.kt
 package constrainingVisibilityExercise1
 
-// TODO
+class Alien(val name: String, val species: String, var planet: String) {
+    fun movePlanet(destPlanet: String): Unit {
+        this.planet = destPlanet    // Correct!!
+//        this.planet = destPlanet
+    }
+
+    override fun toString(): String {
+        return "Alien $name, $species: $planet"
+    }
+}
 
 fun main() {
-  // TODO
+    val alien1 = Alien("Arthricia", "Cat Person", "PurgePlanet")
+    println(alien1)
+    alien1.movePlanet("Earth C-137")
+    println(alien1)
+
+    val alien2 = Alien("Dale", "Giant", "Gearworld")
+    println(alien2)
+    alien2.movePlanet("Parblesnops")
+    println(alien2)
 }
 /* Expected output:
 Alien Arthricia, Cat Person: PurgePlanet
