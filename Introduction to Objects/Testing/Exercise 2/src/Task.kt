271ed8a9145ac1eed37fb6cd25c13e53e6ebd0b5
josealonso <jose.alonso.programmer@gmail.com>
2023-11-14 16:41:10 +0100

    More very basic exercises.

--- Introduction to Objects/Testing/Exercise 2/src/Task.kt
+++ Introduction to Objects/Testing/Exercise 2/src/Task.kt
@@ -1,10 +1,11 @@
 // Testing/Task2.kt
 package testingExercise2
+
 import atomictest.eq
 import atomictest.neq
 
-fun sum(a: Int, b: Int, c: Int): Int = TODO()
+fun sum(a: Int, b: Int, c: Int): Int = a + b + c
 
 fun main() {
-  TODO()
+    sum(1, 2, 3) eq 6
 }
--- Introduction to Objects/Testing/Exercise 3/src/Task.kt
+++ Introduction to Objects/Testing/Exercise 3/src/Task.kt
@@ -4,7 +4,7 @@ import atomictest.eq
 
 fun squareArea(x: Int) = x * x
 
-fun rectangleArea(x: Int, y: Int) = x * x
+fun rectangleArea(x: Int, y: Int) = x * y
 
 fun trapezoidArea(x: Int, y: Int, h: Int) = h / 2 * (x + y)
 
@@ -13,4 +13,5 @@ fun main() {
   squareArea(2) eq 4
   squareArea(5) eq 25
   // add more tests
+  rectangleArea(2,4) eq 8
 }
