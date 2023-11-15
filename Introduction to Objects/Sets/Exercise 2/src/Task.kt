cf0ae8c553218934d0d235c4219a4c13b95932ca
josealonso <jose.alonso.programmer@gmail.com>
2023-11-15 02:07:23 +0100

    Using sets. Special mention to the intersect infix function.

--- Introduction to Objects/Sets/Exercise 2/src/Task.kt
+++ Introduction to Objects/Sets/Exercise 2/src/Task.kt
@@ -1,15 +1,22 @@
 // Sets/Task2.kt
 package setsExercise2
+
 import kotlin.random.Random
 
 fun experiment(): Int {
-  TODO()
+    var counter = 0
+    val set = mutableSetOf<Int>()
+    while (set.size < 25) {
+        set.add(Random.nextInt(25) + 1)
+        counter++
+    }
+    return counter
 }
 
 fun main() {
-  val counts = mutableListOf<Int>()
-  repeat(1000) {
-    counts += experiment()
-  }
-  println(counts.average())
+    val counts = mutableListOf<Int>()
+    repeat(1000) {
+        counts += experiment()
+    }
+    println(counts.average())
 }
