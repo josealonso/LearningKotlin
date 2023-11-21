f4ad483765a530e8a6cee778166152630ae28131
José Ramón (JR) <jose.alonso.programmer@gmail.com>
2023-11-22 00:00:02 +0100

    Java equivalent to data classes

--- Introduction to Objects/DataClasses.kt
+++ Introduction to Objects/DataClasses.kt
@@ -1,3 +1,25 @@
+// Learn about classes, properties and data classes and then rewrite the following Java code to Kotlin:
+
+// public class Person {
+
+//     private final String name;
+//     private final int age;
+
+// ​    public Person(String name, int age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+
+// ​    public String getName() {
+//         return name;
+//     }
+
+//     public int getAge() {
+//         return age;
+//     }
+// }
+
+
 data class Person(val name: String, val age: Int)
 
 fun getPeople(): List<Person> {
