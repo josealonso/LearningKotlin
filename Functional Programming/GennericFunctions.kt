63c812463fa6a713c20b4fe36c56d9e1fc076654
José Ramón (JR) <jose.alonso.programmer@gmail.com>
2023-11-23 17:39:07 +0100

    Generic functions.

---
+++ Functional Programming/GennericFunctions.kt
@@ -0,0 +1,28 @@
+// Make the code compile by implementing a partitionTo function that splits a collection into two collections according to the predicate.
+// There is a partition() function in the standard library that always returns two newly created lists. Write a function that splits the collection 
+// into two collections given as arguments. The signature of the toCollection() function from the standard library might help you.
+ 
+fun <T, C : MutableCollection<T>> Collection<T>.partitionTo(first: C, second: C, predicate: (T) -> Boolean): Pair<C, C> {
+    for (element in this) {
+        if (predicate(element)) {
+            first.add(element)
+        } else {
+            second.add(element)
+        }
+    }
+    return Pair(first, second)
+}
+
+fun partitionWordsAndLines() {
+    val (words, lines) = listOf("a", "a b", "c", "d e")
+            .partitionTo(ArrayList(), ArrayList()) { s -> !s.contains(" ") }
+    check(words == listOf("a", "c"))
+    check(lines == listOf("a b", "d e"))
+}
+
+fun partitionLettersAndOtherSymbols() {
+    val (letters, other) = setOf('a', '%', 'r', '}')
+            .partitionTo(HashSet(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z' }
+    check(letters == setOf('a', 'r'))
+    check(other == setOf('%', '}'))
+}
