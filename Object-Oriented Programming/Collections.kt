a2e234b20f85f45778a30c6416965d5ee018dfcb
José Ramón (JR) <jose.alonso.programmer@gmail.com>
2023-11-23 13:47:52 +0100

    All, any, and other predicates

--- Object-Oriented Programming/Collections.kt
+++ Object-Oriented Programming/Collections.kt
@@ -17,7 +17,25 @@ fun Shop.getCustomerCities(): Set<City> =
 // Find the customers living in a given city
 fun Shop.getCustomersFrom(city: City): List<Customer> =
         customers.filter { it.city == city }
-        
+
+// Implement the following functions using all, any, count, find
+
+// Return true if all customers are from a given city
+fun Shop.checkAllCustomersAreFrom(city: City): Boolean =
+    customers.all { it.city == city }
+
+// Return true if there is at least one customer from a given city
+fun Shop.hasCustomerFrom(city: City): Boolean =
+    customers.any { it.city == city }
+
+// Return the number of customers from a given city
+fun Shop.countCustomersFrom(city: City): Int =
+    customers.count { it.city == city }
+
+// Return a customer who lives in a given city, or null if there is none
+fun Shop.findCustomerFrom(city: City): Customer? =
+    customers.find { it.city == city }
+    
 //  ---------------------------------------------------------------------------------
 
 data class Shop(val name: String, val customers: List<Customer>)
