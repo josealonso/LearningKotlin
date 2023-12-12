e71afc5a99248fe420777316bcd4140ec7f35e81
José Ramón (JR) <jose.alonso.programmer@gmail.com>
2023-12-12 15:47:21 +0100

    Fuctions overview.

---
+++ Functions.kt
@@ -0,0 +1,17 @@
+
+// Funcions can be single expressions, in this case "return" is not used
+fun greet(name: String): String = "Hello $name"
+
+println(greet("Bob"))
+
+// Default arguments
+fun greet(name: String, msg: String = "Hello"): String = "$msg $name"
+println(greet("Bob", "Good morning"))
+println(greet("Bob"))   // Hello Bob
+
+// Named arguments
+// You can mix positional and named arguments
+val message = greet(msg = "Good morning", name = "Bob")
+
+// varargs for several parameters. It's similar in Java.
+// vararg is a keyword in Kotlin
