b488654ec9050ef54f2bf3af0aac1c04784b18d5
José Ramón (JR) <jose.alonso.programmer@gmail.com>
2023-12-12 17:03:45 +0100

    Moved file.

---
+++ SmartCasting.kt
@@ -0,0 +1,28 @@
+
+// Argument matching and auto casting
+
+// Java
+// public static void process(Object input) {
+//   if(input instanceOf String) { 
+//     System.out.println(
+//             "got a string of length " + ((String) input.length);
+//  } else {
+//    ..
+//  }
+//}
+
+fun process(input: Any): String {
+  val result = when(input) {
+    1 -> "one"
+    in 13..19 -> "teen"
+    is String -> "got a string of length ${input.length}"
+    else -> "whatever"
+  }
+  return result
+}
+
+fun main() {
+  println(process(1))
+  println(process(14))
+  println(process("hello"))
+}
